8dada6d2265717ebce4355acdb26901f12e1c2fe
Edmund Korley <ekorley@student.42.us.org>
2016-09-22 17:46:03 -0700

    Singular header for libft function prototypes

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: ekorley <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/09/21 11:34:32 by ekorley           #+#    #+#             */
-/*   Updated: 2016/09/22 13:19:21 by ekorley          ###   ########.fr       */
+/*   Updated: 2016/09/22 17:17:11 by ekorley          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,16 @@
 # include <stdlib.h>
 # include <unistd.h>
 # include <string.h>
-# include "ft_memory.h"
-# include "ft_strings.h"
-# include "ft_lists.h"
-# include "ft_streams.h"
+
+void	ft_bzero(void *s, size_t n);
+void	*ft_memset(void *b, int c, size_t len);
+void	*ft_memcpy(void *dest, const void *src, size_t n);
+void	*ft_memccpy(void *dest, const void *src, int c, size_t n);
+void	*ft_memmove(void *dest, const void *src, size_t n);
+void	*ft_memchr(const void *s, int c, size_t n);
+void	*ft_memrchr(const void *s, int c, size_t n);
+int		ft_memcmp(const void *s1, const void *s2, size_t n);
+void	*ft_memalloc(size_t size);
+void	ft_memdel(void **ap);
 
 #endif
