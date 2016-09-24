297750de2b0cffbd4207ab5fdc7bcf8bcef011b7
Edmund Korley <ekorley@student.42.us.org>
2016-09-23 18:25:34 -0700

    Bool returning functions: `ft_isalnum`, `ft_isalpha`, `ft_isascii`, `ft_isdigit`, `ft_isprint`.

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: ekorley <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/09/21 11:34:32 by ekorley           #+#    #+#             */
-/*   Updated: 2016/09/22 17:17:11 by ekorley          ###   ########.fr       */
+/*   Updated: 2016/09/23 18:19:58 by ekorley          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@
 # include <stdlib.h>
 # include <unistd.h>
 # include <string.h>
+# define WS(x) (x == ' ' || x == '\t' || x == '\n')
 
 void	ft_bzero(void *s, size_t n);
 void	*ft_memset(void *b, int c, size_t len);
@@ -27,5 +28,15 @@ void	*ft_memrchr(const void *s, int c, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
 void	*ft_memalloc(size_t size);
 void	ft_memdel(void **ap);
+void	ft_strlen(const char *s);
+int		ft_tolower(int c);
+int		ft_toupper(int c);
+int		ft_atoi(const char *str);
+char	*ft_itoa(int n);
+int		ft_isalpha(int c);
+int		ft_isdigit(int c);
+int		ft_isalnum(int c);
+int		ft_isascii(int c);
+int		ft_isprint(int c);
 
 #endif
