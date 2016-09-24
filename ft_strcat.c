fcf947b7c71c846b740fe67fb616a40dbd991635
Edmund Korley <ekorley@student.42.us.org>
2016-09-24 09:35:54 -0700

    Use `strcpy` instead of `memcpy` for `ft_strcat`

--- ft_strcat.c
+++ ft_strcat.c
@@ -6,7 +6,7 @@
 /*   By: ekorley <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/09/24 05:09:39 by ekorley           #+#    #+#             */
-/*   Updated: 2016/09/24 05:38:42 by ekorley          ###   ########.fr       */
+/*   Updated: 2016/09/24 09:32:59 by ekorley          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,10 @@
 
 char	*ft_strcat(char *s1, const char *s2)
 {
-	size_t		t;
 	void		*rest_of_s1;
 	char		*result;
 
-	t = sizeof(char) * (ft_strlen(s2) + 1);
 	rest_of_s1 = ft_memchr(s1, 0, 1000);
-	result = (char *)ft_memcpy(rest_of_s1, s2, t);
+	result = ft_strcpy(rest_of_s1, s2);
 	return (result);
 }
