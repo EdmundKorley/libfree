e750309a41615c9408d47c674a4d8d4d80027ecb
Edmund Korley <ekorley@student.42.us.org>
2016-09-27 09:35:12 -0700

    More robust iterative ft_putnbr

--- ft_putnbr.c
+++ ft_putnbr.c
@@ -6,13 +6,32 @@
 /*   By: ekorley <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/09/25 08:00:16 by ekorley           #+#    #+#             */
-/*   Updated: 2016/09/26 11:29:52 by ekorley          ###   ########.fr       */
+/*   Updated: 2016/09/27 09:17:48 by ekorley          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_putnbr(int n)
+void	ft_putnbr(int nb)
 {
-	ft_putstr(ft_itoa(n));
+	if (nb > 2147483647 || nb < -2147483648)
+		return ;
+	if (nb == -2147483648)
+	{
+		ft_putstr("-2147483648");
+		return ;
+	}
+	if (nb < 0)
+	{
+		nb = nb * -1;
+		ft_putchar('-');
+	}
+	if (nb < 10)
+	{
+		ft_putchar(nb + 48);
+		return ;
+	}
+	ft_putnbr(nb / 10);
+	ft_putchar((nb % 10) + 48);
+	return ;
 }
--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,13 +6,32 @@
 /*   By: ekorley <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/09/25 08:09:44 by ekorley           #+#    #+#             */
-/*   Updated: 2016/09/26 11:15:01 by ekorley          ###   ########.fr       */
+/*   Updated: 2016/09/27 09:18:57 by ekorley          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_putnbr_fd(int n, int fd)
+void	ft_putnbr_fd(int nb, int fd)
 {
-	ft_putstr_fd(ft_itoa(n), fd);
+	if (nb > 2147483647 || nb < -2147483648)
+		return ;
+	if (nb == -2147483648)
+	{
+		ft_putstr_fd("-2147483648", fd);
+		return ;
+	}
+	if (nb < 0)
+	{
+		nb = nb * -1;
+		ft_putchar_fd('-', fd);
+	}
+	if (nb < 10)
+	{
+		ft_putchar_fd(nb + 48, fd);
+		return ;
+	}
+	ft_putnbr_fd(nb / 10, fd);
+	ft_putchar_fd((nb % 10) + 48, fd);
+	return ;
 }
