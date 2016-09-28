4ff8932704935c388fc5d4974e32bf5c8568705d
Edmund Korley <ekorley@student.42.us.org>
2016-09-28 10:29:49 -0700

    Protypes and briefs for routines to implement

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: ekorley <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/09/21 11:34:32 by ekorley           #+#    #+#             */
-/*   Updated: 2016/09/26 10:58:13 by ekorley          ###   ########.fr       */
+/*   Updated: 2016/09/28 10:28:29 by ekorley          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,12 +20,9 @@
 # include <unistd.h>
 # include <string.h>
 
-typedef	struct		s_list
-{
-	void			*content;
-	size_t			content_size;
-	struct s_list	*next;
-}					t_list;
+/*
+**	These set of functions manipulate raw memory at the byte level.
+*/
 
 void				ft_bzero(void *s, size_t n);
 void				*ft_memset(void *b, int c, size_t len);
@@ -37,17 +34,29 @@ void				*ft_memrchr(const void *s, int c, size_t n);
 int					ft_memcmp(const void *s1, const void *s2, size_t n);
 void				*ft_memalloc(size_t size);
 void				ft_memdel(void **ap);
+void				*ft_memrotate(void *buffer, size_t size, int torotate);
+
+/*
+**	These set of functions manipulate strings,
+**	and primitive data types like ints and chars.
+*/
+
 size_t				ft_strlen(const char *s);
 int					ft_tolower(int c);
 int					ft_toupper(int c);
+int					ft_ctoi(char letter);
 int					ft_atoi(const char *str);
 char				*ft_itoa(int n);
+int					ft_maxint(int num1, int num2);
 int					ft_lenofint(int n);
 int					ft_isalpha(int c);
+int					ft_isupper(int c);
+int					ft_islower(int c);
 int					ft_isdigit(int c);
 int					ft_isalnum(int c);
 int					ft_isascii(int c);
 int					ft_isprint(int c);
+int					ft_ismiscellanous(int c);
 char				*ft_strdup(const char *s1);
 char				*ft_strcpy(char *dst, const char *src);
 char				*ft_strncpy(char *dst, const char *src, size_t len);
@@ -73,6 +82,27 @@ char				*ft_strsub(char const *s, unsigned int start, size_t len);
 char				*ft_strjoin(char const *s1, char const *s2);
 char				*ft_strtrim(char const *s);
 char				**ft_strsplit(char const *s, char c);
+
+/*
+**	These set of functions implement useful routines on integer arrays.
+*/
+
+void				ft_tabforeach(int *tab, int len, void (*f)(int));
+int					*ft_tabmap(int *tab, int len, int (*f)(int));
+int					*ft_tabreduce(int *tab, int len, int (*f)(int, int));
+int					*ft_tabfilter(int *tab, int len, int (*f)(int));
+int					ft_tabsum(int *tab, int len);
+int					ft_tabmax(int *tab, int len);
+int					ft_tabmin(int *tab, int len);
+int					ft_tabmode(int *tab, int len);
+int					ft_tabmean(int *tab, int len);
+int					ft_tabrotate(int *tab, int len, int torotate);
+
+/*
+**	These set of functions involving sending data to different streams,
+**	either standard out or a specified stream.
+*/
+
 void				ft_putchar(char c);
 void				ft_putstr(char const *s);
 void				ft_putendl(char const *s);
@@ -81,11 +111,51 @@ void				ft_putchar_fd(char c, int fd);
 void				ft_putstr_fd(char const *s, int fd);
 void				ft_putendl_fd(char const *s, int fd);
 void				ft_putnbr_fd(int n, int fd);
+
+/*
+**	These set of functions involve creating and manipulating linked lists.
+*/
+
+typedef	struct		s_list
+{
+	void			*content;
+	size_t			content_size;
+	struct s_list	*next;
+}					t_list;
+
 t_list				*ft_lstnew(void const *content, size_t content_size);
 void				ft_lstdelone(t_list **alst, void (*del)(void *, size_t));
 void				ft_lstdel(t_list **alst, void(*del)(void *, size_t));
 void				ft_lstadd(t_list **alst, t_list *new);
 void				ft_lstiter(t_list *lst, void (*f)(t_list *elem));
 t_list				*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem));
+
+/*
+**	These set of functions involve creating and manipulating binary trees.
+*/
+
+typedef	struct		s_btree
+{
+	struct s_btree	*left;
+	struct s_btree	*right;
+	void			*item;
+}					t_btree;
+
+t_btree				*btree_create_node(void *item);
+void				btree_apply_infix(t_btree *root, void (*applyf)(void *));
+void				btree_apply_prefix(t_btree *root, void (*applyf)(void *));
+void				btree_apply_suffix(t_btree *root, void (*applyf)(void *));
+int					btree_level_count(t_btree *root);
+void				*btree_search_item(t_btree *root, void *data_ref,
+					int (*cmpf)(void*, void*));
+
+/*
+**	These set of functions involve creating and manipulating stacks.
+*/
+
+/*
+**	These set of functions involve creating and manipulating dynamic arrays,
+**	the kind you might find in a higher level language like Python.
+*/
 
 #endif
