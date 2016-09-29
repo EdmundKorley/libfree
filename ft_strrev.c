e5e6bd8796e2c49407a7ded108153482c261fcb9
Edmund Korley <ekorley@student.42.us.org>
2016-09-29 10:13:21 -0700

    String reverse routine

---
+++ ft_strrev.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strrev.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ekorley <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/08/12 10:17:51 by ekorley           #+#    #+#             */
+/*   Updated: 2016/09/29 08:50:46 by ekorley          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+/*
+**	Reverses a string non-destructively.
+*/
+
+char		*ft_strrev(char *str)
+{
+	int			i;
+	int			length;
+	char		tmp;
+
+	if (str)
+	{
+		i = 0;
+		length = ft_strlen(str);
+		while (i < length / 2)
+		{
+			tmp = str[i];
+			str[i] = str[length - i - 1];
+			str[length - i - 1] = tmp;
+			i++;
+		}
+	}
+	return (str);
+}
