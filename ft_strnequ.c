5d825fc946118bab87336913953f34cb2631ecb4
Edmund Korley <ekorley@student.42.us.org>
2016-10-01 19:24:14 -0700

    Overlay strnequ over strncmp

--- ft_strnequ.c
+++ ft_strnequ.c
@@ -6,7 +6,7 @@
 /*   By: ekorley <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/09/24 19:45:16 by ekorley           #+#    #+#             */
-/*   Updated: 2016/09/29 15:54:52 by ekorley          ###   ########.fr       */
+/*   Updated: 2016/10/01 18:39:56 by ekorley          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,16 +18,10 @@
 
 int		ft_strnequ(char const *s1, char const *s2, size_t n)
 {
-	size_t		i;
-
-	if (!s1 || !s2)
+	if (s1 == NULL || s2 == NULL)
 		return (0);
-	i = 0;
-	while (s1[i] && s2[i] && i < n)
-	{
-		if (s1[i] != s2[i])
-			return (0);
-		i++;
-	}
-	return (1);
+	if (ft_strncmp(s1, s2, n) == 0)
+		return (1);
+	else
+		return (0);
 }
